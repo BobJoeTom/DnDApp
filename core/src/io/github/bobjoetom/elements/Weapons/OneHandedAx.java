e9de4f7b286a832b298a7cac1eea3d7c5346a7bd
BobJoeTom <bobjoetom@gmail.com>
2016-06-30 23:11:40 -0400

    First Commit

---
+++ core/src/io/github/bobjoetom/elements/Weapons/OneHandedAx.java
@@ -0,0 +1,24 @@
+package io.github.bobjoetom.elements.Weapons;
+
+/**
+ * Created by Brenden on 6/30/2016.
+ */
+public class OneHandedAx extends Weapon{
+    public void OneHandedAx(){
+        setName("One Handed Ax");
+        setDiscription("This is an ax");
+        setTexturePath("OneHandAX.jpeg");
+        setDamage(99999);
+    }
+
+    @Override
+    public void speacial() {
+    /*Methods that can be used
+    getUser(); retuens User of Item
+    getVictim(); gets victim of Weapon
+
+    each of these return a character object IE the user or Vitim
+
+     */
+    }
+}
